e5435d94f4fa7ccb65ceab098cbefff2165ecbe5
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2023-02-27 15:44:50 -0800

    Finish WIP. Code now checks whether the session is an early session with a bad labcams logfile and uses the modified function for aligning the trial data to the video frames.

--- analyze_behavior.py
+++ analyze_behavior.py
@@ -10,9 +10,9 @@ Behavioral Analyses for chipmunk_solo
 # %% Import functions
 
 import sys
-sys.path.insert(0, '/Users/gabriel')  # wherever the parent directory for chiCa lives. just need to run it once
+# sys.path.insert(0, r'C:\Users\Anne\chiCa')  # wherever the parent directory for chiCa lives. just need to run it once
 from chiCa import *
-import os
+
 
 # %% Convert .mat files to .h5
 
@@ -22,57 +22,46 @@ converted_files = convert_specified_behavior_sessions(file_names)
 # %% Align behavior to video data
 
 # Separate sessions with bad labcams logfiles
-earlyGRBsessions = [session for session in sorted(os.listdir('/Users/gabriel/data/GRB001'))
-                    if not session.startswith('.DS_Store') and '20230223' not in session and '20230224' not in session]
+earlyGRBsessions = ['20230126_110314',
+                     '20230127_114504',
+                     '20230130_121533',
+                     '20230131_113044',
+                     '20230201_131437',
+                     '20230201_131715',
+                     '20230202_123237',
+                     '20230202_123648',
+                     '20230203_133526',
+                     '20230207_153632',
+                     '20230208_113843',
+                     '20230209_115759',
+                     '20230210_132751',
+                     '20230213_132401',
+                     '20230217_132430',
+                     '20230221_112843']
 
 file_names = pick_files_multi_session("chipmunk", "*.camlog", "BackStereo")
 
+# Align data
 for camlog_file in file_names:
-    if str.split(camlog_file, '/')[5] in earlyGRBsessions:
-        print('Using modified alignment function for early GRB sessions.')
+    if str.split(camlog_file, '\\')[5] in earlyGRBsessions:
+        t = str.split(camlog_file, '\\')[5]
+        print('------------------------------')
+        print(f'Using early sessions alignment function for {t}.')
         try:
-            print(early)
-            break
             align_behavioral_video_earlyGRBsessions(camlog_file)
         except:
             print("There was an issue with the camlog file for the current early session:", camlog_file)
             print("Continuing to the next one...")
             continue
     else:
-        print('Using default alignment function.')
+        t = str.split(camlog_file, '\\')[5]
+        print('------------------------------')
+        print(f'Using default alignment function for {t}.')
         try:
-            print('normal')
-            break
             align_behavioral_video(camlog_file)
         except:
             print("There was an issue with the camlog file for the current session:", camlog_file)
             print("Continuing to the next one...")
             continue
 
-# %% Align trial data to frame times
 
-# this is necessary because I screwed up the logfile from labcams for the first couple of sessions 
-# this would normally be done by the align_behavioral_video function in the previous section
-
-from labcams import parse_cam_log
-import numpy as np
-import pandas as pd
-
-log, comm = parse_cam_log(
-    'c:/Users/Anne/data/GRB001/20230208_113843/chipmunk/GRB001_20230208_113843_chipmunk_DemonstratorAudiTask_FrontStereoView_00000000.camlog')
-
-# spaces = [pos for pos, char in enumerate(comments[0]) if char == ' '] #Find all the empty spaces that separate the words
-# camera_name = comments[0][spaces[1]+1: spaces[2]] #The name comes between the second and the third space
-
-ts = []
-a = []
-for l in comm:
-    l = l.strip('#')
-
-    if 'trial_start' in l:
-        a = [int(l.split(':')[-1]), int(l.split(',')[0])]  # first give me trial num, second gives me frame
-    if 'trial_end' in l:
-        a.append(int(l.split(',')[0]))
-        ts.append(a)
-
-Front_times = pd.DataFrame(np.stack(ts), columns=['itrial', 'onset_frame', 'offset_frame'])
