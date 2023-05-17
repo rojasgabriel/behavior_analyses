2f71c8d5843d260e7724735d0967b8600fa656d4
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2023-05-17 15:58:24 -0700

    Fix function for loading data and added functionality for quickly assessing the framerate of the video being analyzed

--- TIV/analysis.py
+++ TIV/analysis.py
@@ -1,14 +1,16 @@
-from utils import get_filepath, load_dlc_data, load_experiment_data_joao, moving_average
+from utils import get_filepath, load_dlc_data, load_data_from_droplets
 import os
-from glob import glob
-import numpy as np
+from os.path import join as pjoin
 import matplotlib.pyplot as plt
+import numpy as np
 
 #Define basic data lookup parameters
 params = dict({'subject':'JC111', 
                'session':'20230324_131923',
                'subfolders':'*',
                'extension':'.h5'})
+
+data_folder = pjoin(os.path.expanduser('~'),'data')
 
 data_paths = get_filepath(subject = params['subject'], 
                           session = params['session'], 
@@ -39,14 +41,18 @@ lateral_bpts, lateral_dlc_coords_x, lateral_dlc_coords_y, lateral_dlc_coords_lik
 # Reading bottom view data
 bottom_bpts, bottom_dlc_coords_x, bottom_dlc_coords_y, bottom_dlc_coords_likelihood = load_dlc_data(bottom_data_path[0])
 
-#Load trial data
-analysis_folders = [os.path.dirname(r) for r in glob(r'C:/Users/Anne/data/JC111/*/*/*.mptracker.h5')]
-task_folders = [os.path.dirname(r) for r in glob(r'C:/Users/Anne/data/JC111/*/*/*.triallog.h5')]
+#Load trial data and check the frame rate
+sessionfolder = pjoin(data_folder,params['subject'],params['session'])
+trialdata,camlog,camcomm,camtime,eyedata = load_data_from_droplets(sessionfolder)
+print(len(camtime),np.mean(1./np.diff(camtime)))
+plt.figure(figsize=[10,2])
+plt.plot(camtime[:-1],1./np.diff(camtime),
+                'o',alpha = 0.5,clip_on = False,label='camera rate')
+plt.vlines(trialdata['trial_start'],25,30,color='r',lw = 0.5,label='trial start')
+plt.legend()
+plt.ylabel('Frame rate')
+plt.xlabel('Time from camera start')
+plt.ylim([0,np.max(plt.ylim())])
+plt.title(f"{params['subject']} - {params['session']}")
 
-for analysis_folder, task_folder in zip(analysis_folders, task_folders):
-    if not len(analysis_folders) == 1:
-        sessiondata = []
-        sessiondata.append(trialdata)
-    trialdata = load_experiment_data_joao(analysis_folder, task_folder)
-
-plt.plot(moving_average(trialdata['rewarded']))
+# plt.plot(moving_average(trialdata['rewarded']))
--- TIV/utils.py
+++ TIV/utils.py
@@ -39,17 +39,64 @@ def load_dlc_data(data_path):
         return None, None, None, None
     
 
-def load_experiment_data_joao(analysis_folder, task_folder): #specific to Joao's experiment output
-    with h5.File(glob(pjoin(analysis_folder,'*.mptracker.h5'))[0],'r') as pupildata:
-        diam = pupildata['diameter'][:]
-    camlog,comm = parse_cam_log(glob(pjoin(task_folder,'*.camlog'))[0])
-    sync = unpackbits(np.array(camlog['var2']))[0][2]
-    trialdata = pd.read_hdf(glob(pjoin(task_folder,'*triallog.h5'))[0])
+def get_times_for_state_onset(log, state = 'stim'):
+    statetimes = []
+    for i,l in log.iterrows():
+        t = np.nan
+        a = l['task_states']
+        for s in a: # for each state
+            if state in s:
+                t = s[-1]
+        statetimes.append(t)
+    return np.array(statetimes)
 
-    trial_onsets = np.array([r.task_start_time + r.task_states[2][-1] for i,r in trialdata.iterrows()])
+def load_data_from_droplets(sessionfolder, 
+                            dropletsfolder = 'DropletsTask', 
+                            read_riglog = False):
     
-    #get the frametimes syncronized with the trial onset.
-    camtime = interp1d(sync[:len(trial_onsets)],trial_onsets+0.3, fill_value = 'extrapolate')(np.arange(len(diam)))
+    eyedata = {}
+    camgpio = []
+    mptrackerfile = glob(pjoin(sessionfolder,'*','*.mptracker.h5'))
+    if len(mptrackerfile):
+        with h5.File(mptrackerfile[0],'r') as pupildata:
+            for k in pupildata.keys():
+                eyedata[k] = pupildata[k][:]
+    
+    from labcams import parse_cam_log
+    camlog,camcomm = parse_cam_log(glob(pjoin(sessionfolder,dropletsfolder,'*.camlog'))[0])
+
+    camsyncmethod = 'trial_init'
+    if 'var2' in camlog.columns:
+        camgpio = unpackbits(np.array(camlog['var2'])) # the onsets
+    
+    # check the if the number of cam sync pulses is close to the number of trials if not the cable was not well connected
+    trialdata = pd.read_hdf(glob(pjoin(sessionfolder,dropletsfolder,'*triallog.h5'))[0])
+    if len(camgpio):
+        if np.abs(len(camgpio) - len(trialdata))<5: # only if recorded most trials on the camera
+            if 2 in camgpio[0].keys():
+                gpiosync = camgpio[0][2]
+                # check here if the trialstart was connected or it was the stim 
+                if np.mean(camgpio[1][2]-camgpio[0][2]) > 15:
+                    camsyncmethod='stim'
+                    #print('Using the stim command to sync {0}.'.format(sessionfolder))
+    # otherwise use the network command
+    if not 'sync' in locals():
+        # then use the network command
+        sync = []
+        for c in camcomm:
+            if 'trial_start' in c:
+                sync.append(int(c.strip('# ').split(',')[0]))
+        camsyncmethod = 'trial_init'
+        print('Using the network command to sync {0}.'.format(sessionfolder))
+    trial_onsets = get_times_for_state_onset(trialdata,camsyncmethod)
+    if camsyncmethod == 'trial_init':
+        trial_onsets[:] = 0.
+    trial_onsets += np.array(trialdata.task_start_time)  # add the start of each trial
+    # get the frametimes syncronized with the trial onset.
+    nframes = camlog.frame_id.iloc[-1]+1 # frame_id starts at zero
+    if 'diameter' in eyedata.keys(): # use the size of diameter if available
+        nframes = len(eyedata['diameter'])
+    camtime = interp1d(sync[:len(trial_onsets)],trial_onsets, fill_value = 'extrapolate')(np.arange(nframes))
 
     rewarded_side = np.array(trialdata.rewarded_side == 'left',dtype = int)
     rewarded_side[rewarded_side==0] = 2
@@ -76,10 +123,10 @@ def load_experiment_data_joao(analysis_folder, task_folder): #specific to Joao's
                       'timeout_time':timeout_time,
                       'stim_intensity':stim_intensity,
                       'stim_rates':stim_rates,
-                      'pupil_diameter':diam,
+                      'pupil_diameter':eyedata['diameter'],
                       'frame_times':camtime})
     
-    return trialdata
+    return trialdata,camlog,camcomm,camtime, eyedata
 
 
 def moving_average(a, n=50): #n = window to average across
